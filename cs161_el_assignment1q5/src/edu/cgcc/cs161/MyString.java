00b54590857c1df5a0c1f5a61a7200f340f5e7dd
Ethlyo <77252686+Ethlyo@users.noreply.github.com>
2021-01-10 20:28:15 -0800

    Add files via upload

---
+++ cs161_el_assignment1q5/src/edu/cgcc/cs161/MyString.java
@@ -0,0 +1,8 @@
+package edu.cgcc.cs161;
+//class and object for q5
+public class MyString {
+
+	public void sayHelloWorld() {
+		System.out.print("Hello World!");
+	}
+}
---
+++ cs161_el_assignment1q5/src/edu/cgcc/cs161/q5.java
@@ -0,0 +1,28 @@
+package edu.cgcc.cs161;
+
+public class q5 {
+	//Program Name: Assignment1q5
+	//Author: Ethan Lyons
+	//Class: CS2161 Winter 2021
+	//Date: 01/10/2021
+	//Description: Prints "Hello World!"
+
+	/*Psuedocode
+	* Program start
+	* Create object MyString with method sayHelloWorld that prints "Hello World!" to console
+	* Access object
+	* use sayHelloWorld to print "Hello World!" to console
+	* Program end
+	*/
+
+	public static void main(String[] args) {
+	MyString hello = new MyString();
+	hello.sayHelloWorld(); 
+	}
+
+}
+
+
+/* Footer
+*Hello World!
+*/
