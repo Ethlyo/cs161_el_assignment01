a265cca0415fbab4e577cfbd7e76b21d282652d6
Ethlyo <77252686+Ethlyo@users.noreply.github.com>
2021-01-10 20:29:03 -0800

    Add files via upload

---
+++ cs161_el_assignment1q6/src/edu/cgcc/cs161/calculator.java
@@ -0,0 +1,11 @@
+package edu.cgcc.cs161;
+//class and object for q6
+public class calculator extends q6 {
+
+	static float a=20;
+	static float b=1.5f;
+	
+	public void calculate() {
+		System.out.print(a*b);
+	}
+}
---
+++ cs161_el_assignment1q6/src/edu/cgcc/cs161/q6.java
@@ -0,0 +1,25 @@
+package edu.cgcc.cs161;
+//Program Name: Assignment1q6
+//Author: Ethan Lyons
+//Class: CS2161 Winter 2021
+//Date: 01/10/2021
+//Description: Prints "Hello World!"
+
+/*Psuedocode
+* Program start
+* create variables a and b with values 20 and 1.5 respectively
+* Create object calculator with method calculate that multiplies a and b before printing to console
+* Access object calulator
+* use calculate to print a*b to console
+* Program end
+*/
+
+public class q6 {
+	public static void main (String[] args) {
+		calculator calc = new calculator();
+        calc.calculate();
+	}
+}
+/* Footer
+* 30.0
+*/
