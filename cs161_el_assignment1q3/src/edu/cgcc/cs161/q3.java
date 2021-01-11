4dcff1f445b8211f2c2f39598aa2015bc1d0bd05
Ethlyo <77252686+Ethlyo@users.noreply.github.com>
2021-01-10 20:26:55 -0800

    Add files via upload

---
+++ cs161_el_assignment1q3/src/edu/cgcc/cs161/q3.java
@@ -0,0 +1,25 @@
+package edu.cgcc.cs161;
+
+public class q3 {
+	//HEADER
+	//Program Name: Assignment1q3
+	//Author: Ethan Lyons
+	//Class: CS2161 Winter 2021
+	//Date: 01/10/2021
+	//Description: Prints "Hello World!"
+
+	/*Psuedocode
+	 * Program start
+	 * Create local variable called Hello that is set to "Hello World!"
+	 * print Hello
+	 * Program end
+	 */
+
+		public static void main(String[] args) {
+			String Hello = "Hello World!";
+			System.out.print(Hello);
+		}
+}
+/* Footer
+*Hello World!
+*/
